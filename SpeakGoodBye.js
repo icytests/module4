ba6fcda406905e1ee518db64d8b2e97c026c8d98
icytests <maki@Maki.local>
2016-07-26 13:59:25 +0300

    4

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,16 +1,10 @@
 
 (function (window){
 var byeSpeaker ={};
-byeSpeaker.name = "John";
-byeSpeaker.name2 = "Jen";
-byeSpeaker.name3 = "Jason";
-byeSpeaker.name4 = "Jim";
 var speakWord = "Good Bye ";
-byeSpeaker.speak = function () {
-  console.log(speakWord + byeSpeaker.name);
-  console.log(speakWord + byeSpeaker.name2);
-  console.log(speakWord + byeSpeaker.name3);
-  console.log(speakWord + byeSpeaker.name4);
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + "" + name);
+
 }
 window.byeSpeaker = byeSpeaker;
 })(window);
--- SpeakHello.js
+++ SpeakHello.js
@@ -1,20 +1,11 @@
 
 (function (window){
 var helloSpeaker ={};
-helloSpeaker.name = "Yaakov";
-helloSpeaker.name2 = "Paul";
-helloSpeaker.name3 = "Frank";
-helloSpeaker.name4 = "Larry";
-helloSpeaker.name5 = "Paula";
-helloSpeaker.name6 = "Laura";
+
 var speakWord = "Hello ";
-helloSpeaker.speak = function () {
-  console.log(speakWord + helloSpeaker.name);
-  console.log(speakWord + helloSpeaker.name2);
-  console.log(speakWord + helloSpeaker.name3);
-  console.log(speakWord + helloSpeaker.name4);
-  console.log(speakWord + helloSpeaker.name5);
-  console.log(speakWord + helloSpeaker.name6);
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + "" + name);
+
 }
 window.helloSpeaker = helloSpeaker;
 })(window);
